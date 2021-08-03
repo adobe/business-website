5aba4a941cdefd522a910d11e3e2aba7653b3027
rofe <github@rofe.com>
2021-08-03 15:58:59 +0200

    chore: fix linting issues

--- blocks/pull-quote/pull-quote.js
+++ blocks/pull-quote/pull-quote.js
@@ -1,12 +1,12 @@
 import {
-    replaceElementType,
+  replaceElementType,
 } from '../block-helpers.js';
 
 export default function decorate($block) {
-    $block.querySelectorAll('p').forEach(($p) => {       
-        // Quote <p> to <h2>
-        if ($p.innerHTML.startsWith('“') && $p.innerHTML.endsWith('”')) {
-            replaceElementType($p, 'blockquote');
-        }
-    });
+  $block.querySelectorAll('p').forEach(($p) => {
+    // Quote <p> to <h2>
+    if ($p.innerHTML.startsWith('“') && $p.innerHTML.endsWith('”')) {
+      replaceElementType($p, 'blockquote');
+    }
+  });
 }
