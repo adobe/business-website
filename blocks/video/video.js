fac064940deca4541d6e8eb46f687761db3d9150
Sean Choi <seanchoi@adobe.com>
2021-08-12 13:27:32 -0600

    Applying eslint rules.

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -9,52 +9,51 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+// eslint-disable-next-line no-redeclare
 /* global window */
 /* eslint-disable import/named, import/extensions */
 
 import { buildFigure } from '../../scripts/scripts.js';
 
 const loadVideo = ($block) => {
-    if($block.classList.contains('is-loaded')) {
-        return;
-    }
-    const poster = $block.querySelector('img') ? `poster="${$block.querySelector('img').src}"` : '';
-    const $a = $block.querySelector('a');
-    const videoSrc = $a.href;
-    const $video = document.createElement('div');
-    const $figure = buildFigure($block.firstChild.firstChild);
-    $video.classList.add('video-wrapper');
-    $video.innerHTML = `<video controls preload="none" ${poster}><source src="${videoSrc}" type="video/mp4"></video>`;
-    $block.innerHTML = '<figure class="figure"></figure>';
-    $block.firstChild.prepend($video);
-    $block.firstChild.append($figure.querySelector('figcaption'));
-    $block.classList.add('is-loaded');
-}
+  if ($block.classList.contains('is-loaded')) {
+    return;
+  }
+  const poster = $block.querySelector('img') ? `poster="${$block.querySelector('img').src}"` : '';
+  const $a = $block.querySelector('a');
+  const videoSrc = $a.href;
+  const $video = document.createElement('div');
+  const $figure = buildFigure($block.firstChild.firstChild);
+  $video.classList.add('video-wrapper');
+  $video.innerHTML = `<video controls preload="none" ${poster}><source src="${videoSrc}" type="video/mp4"></video>`;
+  $block.innerHTML = '<figure class="figure"></figure>';
+  $block.firstChild.prepend($video);
+  $block.firstChild.append($figure.querySelector('figcaption'));
+  $block.classList.add('is-loaded');
+};
 
 const intersectHandler = (entries) => {
-    const entry = entries[0];
-    if (entry.isIntersecting) {
-        if (entry.intersectionRatio >= 0.25) {
-            const $block = entry.target;
-            loadVideo($block);
-        }
-    } else {
-        // if ((entry.intersectionRatio === 0.0) && (adBox.dataset.totalViewTime >= 60000)) {
-        // Error handler placeholder
-        // }
+  const entry = entries[0];
+  if (entry.isIntersecting) {
+    if (entry.intersectionRatio >= 0.25) {
+      const $block = entry.target;
+      loadVideo($block);
     }
+  } else {
+    // if ((entry.intersectionRatio === 0.0) && (adBox.dataset.totalViewTime >= 60000)) {
+    // Error handler placeholder
+    // }
+  }
 };
 
 export default function decorate($block) {
-    window.addEventListener('load', (event) => {
-        let observer;  
-        let options = {
-            root: null,
-            rootMargin: "0px",
-            threshold: [0.0, 0.25]
-        };
-        
-        observer = new IntersectionObserver(intersectHandler, options);
-        observer.observe($block);
-    });
+  window.addEventListener('load', () => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: [0.0, 0.25],
+    };
+    const observer = new IntersectionObserver(intersectHandler, options);
+    observer.observe($block);
+  });
 }
