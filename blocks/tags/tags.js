2efaa0b6996e015916d9061ea7f42ef2517737ac
Katie Sexton <ksexton@adobe.com>
2021-08-30 16:27:19 -0600

    chore: lint

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,5 +1,4 @@
 export default function decorateTags(blockEl) {
-  console.log('hi from decorate tags');
   const tags = blockEl.textContent.split(', ');
   const container = blockEl.querySelector('p');
   container.classList.add('tags-container');
@@ -9,7 +8,6 @@ export default function decorateTags(blockEl) {
     a.setAttribute('href', `../tags/${tag.toLowerCase().replace(' ', '-')}`);
     a.textContent = tag;
     a.classList.add('button');
-    console.log(a);
     container.append(a);
   });
 }
