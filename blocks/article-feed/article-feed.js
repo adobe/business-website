caf0883d927d9387dca66c2496fde6fb71effd92
Katie Sexton <ksexton@adobe.com>
2021-09-17 09:37:01 -0600

    feat: link category in article card

--- blocks/article-feed/article-feed.js
+++ blocks/article-feed/article-feed.js
@@ -73,8 +73,10 @@ async function decorateArticleFeed(articleFeedEl, config, offset = 0) {
           ${pictureTag}
         </div>
         <div class="article-card-body">
-        <p class="article-card-category">${category}</p>
-        <h3>${title}</h3>
+          <p class="article-card-category">
+          <a href="${window.location.origin}/blog/categories/${category}">${category}</a>
+          </p>
+          <h3>${title}</h3>
           <p>${description}</p>
         </div>`;
     articleCards.append(card);
