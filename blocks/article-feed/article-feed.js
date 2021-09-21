076a588d7212ec9f810014ecc91249084c0b4295
Katie Sexton <ksexton@adobe.com>
2021-09-21 16:55:00 -0600

    refactor: beginnings of localization in btn text

--- blocks/article-feed/article-feed.js
+++ blocks/article-feed/article-feed.js
@@ -2,6 +2,7 @@ import {
   readBlockConfig,
   buildArticleCard,
   fetchBlogArticleIndex,
+  fetchVariables,
 } from '../../scripts/scripts.js';
 
 function isCardOnPage(article) {
@@ -66,7 +67,8 @@ async function decorateArticleFeed(articleFeedEl, config, offset = 0) {
     const loadMore = document.createElement('a');
     loadMore.className = 'load-more button secondary';
     loadMore.href = '#';
-    loadMore.innerHTML = 'Load more articles';
+    const vars = await fetchVariables();
+    loadMore.textContent = vars['load-more'];
     articleFeedEl.append(loadMore);
     loadMore.addEventListener('click', (event) => {
       event.preventDefault();
