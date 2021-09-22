05bb2c0efdb6ddcb385816b3602ca24025710ad0
Katie Sexton <ksexton@adobe.com>
2021-09-22 08:42:00 -0600

    refactor: cleanup decorate banner

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -2,43 +2,48 @@ import {
   normalizeHeadings,
 } from '../../scripts/scripts.js';
 
-export default function decorate($block) {
-  const $bannerContents = document.createElement('div');
-  $bannerContents.classList.add('banner-contents');
-  $block.querySelectorAll('a').forEach(async ($a) => {
-    if ($a && $a.href) {
+export default function decorate(block) {
+  const bannerContents = document.createElement('div');
+  bannerContents.classList.add('banner-contents');
+  block.querySelectorAll('a').forEach(async (a) => {
+    if (a && a.href) {
       // content wrapper
-      const $bannerContent = document.createElement('div');
-      $bannerContent.classList.add('content-wrapper');
+      const bannerContent = document.createElement('div');
+      bannerContent.classList.add('content-wrapper');
 
-      // get response from the href URL
-      const response = await fetch(`${$a.href}.plain.html`);
-      const $response = document.createElement('div');
-      $response.innerHTML = await response.text();
-      $block.classList.add('is-loaded');
+      // get response from the URL
+      const { pathname } = new URL(a);
+      const response = await fetch(`${pathname}.plain.html`);
+      if (response.ok) {
+        const responseEl = document.createElement('div');
+        responseEl.innerHTML = await response.text();
+        block.classList.add('is-loaded');
 
-      // creating banner image and text div.
-      const $bannerImage = document.createElement('div');
-      const $bannerText = document.createElement('div');
-      $bannerImage.classList.add('banner-image');
-      $bannerText.classList.add('banner-text');
+        // creating banner image and text div.
+        const bannerImage = document.createElement('div');
+        const bannerText = document.createElement('div');
+        bannerImage.classList.add('banner-image');
+        bannerText.classList.add('banner-text');
 
-      // banner image content
-      const $picture = $response.querySelector('picture');
-      $bannerImage.append($picture);
+        // banner image content
+        const picture = responseEl.querySelector('picture');
+        bannerImage.append(picture);
 
-      // banner text content
-      normalizeHeadings($response, ['h3']);
-      const $link = $response.querySelector('a');
-      $link.classList.add('cta-link');
-      $bannerText.append($response);
+        // banner text content
+        normalizeHeadings(responseEl, ['h3']);
+        const link = responseEl.querySelector('a');
+        link.classList.add('cta-link');
+        bannerText.append(responseEl);
 
-      // appending DOM objects
-      $bannerContent.append($bannerImage);
-      $bannerContent.append($bannerText);
-      $bannerContents.append($bannerContent);
+        // appending DOM objects
+        bannerContent.append(bannerImage);
+        bannerContent.append(bannerText);
+        bannerContents.append(bannerContent);
+        block.innerHTML = '';
+        block.append(bannerContents);
+      } else {
+        block.remove();
+      }
     }
   });
-  $block.innerHTML = '';
-  $block.append($bannerContents);
 }
