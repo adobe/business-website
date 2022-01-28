eb93f362eeb80f54b438efbd88cfbf92a87bebc6
Sean Choi <seanchoi@adobe.com>
2022-01-28 16:38:46 -0700

    adjustments consonant marquee for business website.

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -2,4 +2,5 @@ import defaultExport from '../../consonant/marquee/marquee.js';
 
 export default function decorate(block) {
   defaultExport(block);
+  block.closest('.section-wrapper').classList.add('marquee-container');
 }
