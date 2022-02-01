b3de07f02b7c9efb92c402a42cfc30d5ec8bc2be
Sean Choi <seanchoi@adobe.com>
2022-01-31 17:17:03 -0700

    updates

--- consonant/marquee/marquee.js
+++ consonant/marquee/marquee.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/*
+ * Marquee - v0.0.1
+ */
+
 function decorateButtons(el) {
   const buttons = el.querySelectorAll('em a, strong a');
   buttons.forEach((button) => {
@@ -14,11 +30,11 @@ function decorateButtons(el) {
 
 function decorateText(el) {
   const headings = el.querySelectorAll('h1, h2, h3, h4, h5, h6');
-  const title = headings[headings.length - 1];
-  title.classList.add('heading-XL');
-  title.nextElementSibling.classList.add('body-M');
-  if (headings.length > 1) {
-    headings[0].classList.add('detail-M');
+  const heading = headings[headings.length - 1];
+  heading.classList.add('heading-XL');
+  heading.nextElementSibling.classList.add('body-M');
+  if (heading.previousElementSibling) {
+    heading.previousElementSibling.classList.add('detail-M');
   }
 }
 
