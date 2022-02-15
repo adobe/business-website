13fa34824c11908ca6eee11986a16f8507052200
Sean Choi <seanchoi@adobe.com>
2022-02-15 13:56:13 -0700

    init banner-cta block

---
+++ blocks/banner-cta/banner-cta.js
@@ -0,0 +1,37 @@
+function decorateButtons(el) {
+  const buttons = el.querySelectorAll('em a, strong a, p > a');
+  buttons.forEach((button) => {
+    const parent = button.parentElement;
+    let buttonType;
+    if (parent.nodeName === 'STRONG') {
+      buttonType = ['button', 'primary'];
+    } else if (parent.nodeName === 'EM') {
+      buttonType = ['button'];
+    } else {
+      buttonType = ['cta-link'];
+    }
+    button.classList.add(...buttonType);
+    if (parent.nodeName !== 'P') {
+      parent.insertAdjacentElement('afterend', button);
+      parent.remove();
+    }
+  });
+  if (buttons.length > 0) {
+    buttons[0].closest('p').classList.add('action-area');
+  }
+}
+
+export default function init(block) {
+  block.closest('.section-wrapper').classList.add('banner-cta-container');
+  const children = block.querySelectorAll(':scope > div');
+  const foreground = children[children.length - 1];
+  if (children.length > 1) {
+    children[0].classList.add('background');
+  }
+  foreground.classList.add('foreground', 'container');
+  const heading = block.querySelector('h1, h2, h3, h4, h5, h6');
+  if (heading) {
+    heading.classList.add('heading');
+  }
+  decorateButtons(foreground.querySelector(':scope > div'));
+}
