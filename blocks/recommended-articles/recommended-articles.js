ce76bd97bba67c30d733188a79565f051cf3ef96
Sean Choi <seanchoi@adobe.com>
2022-03-18 14:15:46 -0600

    Fix small recommended article style

--- blocks/recommended-articles/recommended-articles.js
+++ blocks/recommended-articles/recommended-articles.js
@@ -7,7 +7,7 @@ import {
 
 async function decorateRecommendedArticles(recommendedArticlesEl, paths) {
   if (recommendedArticlesEl.classList.contains('small')) {
-    recommendedArticlesEl.parentNode.querySelectorAll('a').forEach((aEl) => {
+    recommendedArticlesEl.closest('.recommended-articles-small-container').querySelectorAll('a').forEach((aEl) => {
       aEl.classList.add('button', 'primary', 'small', 'light');
     });
     recommendedArticlesEl.parentNode.classList.add('recommended-articles-small-content-wrapper');
