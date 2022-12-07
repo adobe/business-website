022034821ada358e2c5d9ad8ca69ae348520198e
Alexandre Capt <acapt@adobe.com>
2022-12-07 15:58:02 +0100

    fix: animations

--- blog/blocks/animation/animation.js
+++ blog/blocks/animation/animation.js
@@ -4,11 +4,12 @@ export default function decorateAnimation(blockEl) {
   const a = blockEl.querySelector('a');
   const parentEl = a.parentNode;
   const href = a.getAttribute('href');
-  const url = new URL(href);
-  const { hostname } = url;
-  let { pathname } = url;
+  if (!href) return;
 
-  if (hostname.includes('hlx.blob.core')) {
+  let pathname = href;
+  if (href.includes('hlx.blob.core')) {
+    const url = new URL(href);
+    pathname = url.pathname;
     // transform links from blob
     const helixId = pathname.split('/')[2];
     const type = href.includes('.mp4') ? 'mp4' : 'gif';
@@ -17,12 +18,12 @@ export default function decorateAnimation(blockEl) {
 
   if (href.includes('.mp4')) {
     const vidEl = `<video playsinline autoplay loop muted>
-      <source src=".${pathname}" type="video/mp4" />
+      <source src="${pathname}" type="video/mp4" />
     </video>`;
     parentEl.innerHTML = vidEl;
   } else if (href.includes('.gif')) {
     const picEl = `<picture>
-      <img src=".${pathname}" />
+      <img src="${pathname}" />
     </picture>`;
     parentEl.innerHTML = picEl;
   }
